a477d283207a3a341ac734fb8bc20914b2b6e099
Rubén González Martínez <ruben.gonzalez.martinez@gmail.com>
2018-11-03 02:04:25 +0100

    Create scenario 'User creates a new article'

---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Creating articles" do
+  scenario "User creates a new article" do
+    visit '/'
+    click_link "New article"
+    fill_in "Title", with: "Creating first article"
+    fill_in "Body", with: "Lorem ipsum"
+    click_button "Create article"
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
+  end
+end
