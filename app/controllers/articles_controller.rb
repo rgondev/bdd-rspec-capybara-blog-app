25b14acda0639cd6e1b1af13942cce0491f01750
Rubén González Martínez <ruben.gonzalez.martinez@gmail.com>
2019-04-28 14:15:04 +0200

    Restrict access with controller actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,15 +22,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own article"
+      redirect_to root_path
+    end
   end
 
   def update
-    if @article.update_attributes(article_params)
-      flash[:success] = "Article has been updated"
-      redirect_to article_path(@article)
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own article"
+      redirect_to root_path
     else
-      flash.now[:danger] = "Article has not been updated"
-      render :new
+      if @article.update_attributes(article_params)
+        flash[:success] = "Article has been updated"
+        redirect_to article_path(@article)
+      else
+        flash.now[:danger] = "Article has not been updated"
+        render :new
+      end
     end
   end
 
